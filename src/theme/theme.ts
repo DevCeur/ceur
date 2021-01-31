8a677490741d483395d86509f0ee1ec408b15300
DevCeur <dev.carlos.umana@gmail.com>
2021-01-31 11:53:11 -0500

    [feat]: customize Heading and Text color component

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,6 +5,18 @@ const theme = extendTheme({
     heading: "'Inter', sans-serif",
     body: "'Inter', sans-serif",
   },
+  components: {
+    Heading: {
+      baseStyle: {
+        color: "gray.800",
+      },
+    },
+    Text: {
+      baseStyle: {
+        color: "gray.600",
+      },
+    },
+  },
 });
 
 export default theme;
