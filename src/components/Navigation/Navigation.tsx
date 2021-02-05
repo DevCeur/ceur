6f64f23254111a939759ba3b85055ff7231b3701
DevCeur <dev.carlos.umana@gmail.com>
2021-02-04 20:27:28 -0500

    [feat]: Add Github link

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,10 @@
 import NextLink from "next/link";
-import { Flex, Heading, HStack, Link, Text } from "@chakra-ui/react";
+
+import { Flex, Heading, HStack, Link } from "@chakra-ui/react";
+import { AiFillGithub } from "react-icons/ai";
+
+import { ROUTE } from "utils/enum";
+
 import Navlink from "components/Navlink";
 
 const Navigation = () => {
@@ -20,18 +25,26 @@ const Navigation = () => {
         spacing={6}
         justify={{ base: "space-between", md: "flex-start" }}
       >
-        <NextLink href="/" passHref>
+        <NextLink href={ROUTE.HOME} passHref>
           <Link _hover={{ textDecor: "none" }}>
             <Heading as="h2">Carlos U.</Heading>
           </Link>
         </NextLink>
         <HStack pl={{ base: 0, md: 6 }} spacing={{ base: 0, md: 4 }}>
-          <Navlink href="/">Home</Navlink>
-          <Navlink href="/blog">Blog</Navlink>
+          <Navlink href={ROUTE.HOME}>Home</Navlink>
+          <Navlink href={ROUTE.BLOG}>Blog</Navlink>
         </HStack>
       </HStack>
       <HStack display={{ base: "none", md: "flex" }}>
-        <Text>Social Links</Text>
+        <Link
+          fontSize="3xl"
+          color="gray.400"
+          href="https://www.github.com/DevCeur"
+          isExternal
+          _hover={{ color: "blue.500" }}
+        >
+          <AiFillGithub />
+        </Link>
       </HStack>
     </Flex>
   );
---
+++ src/utils/enum.ts
@@ -0,0 +1,5 @@
+export const ROUTE = {
+  HOME: "/",
+  BLOG: "/blog",
+  ABOUT: "/about",
+};
