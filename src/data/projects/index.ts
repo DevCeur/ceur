65c43d4eb095de0a0eec28ee7883bfb01479181d
DevCeur <dev.carlos.umana@gmail.com>
2021-03-08 12:18:06 -0500

    [feat]: Fix URLs on projects

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -1,18 +1,16 @@
 export const projects = [
   {
     id: 1,
-    name: "Covid Dashboard  &#128137;",
+    name: "Psychify &#129419;",
     description:
-      "A simple covid dashboard to visualize stats pero country or globally",
-    image: "/images/projects/covid-dashboard.png",
-    url: "https://github.com/DevCeur/covid-dashboard",
+      "One of my biggest personal projects, a platform to find therapists, and book sessions with them.",
+    url: "https://psychify.vercel.app/sign-in",
   },
   {
     id: 2,
-    name: "NextJs Page Transitions",
-    image: "/images/projects/covid-dashboard.png",
+    name: "Covid Dashboard  &#128137;",
     description:
-      "Example of how to implement page transitions using framer-motion",
-    url: "https://github.com/DevCeur/nextjs-basic-paga-transitions",
+      "A simple covid dashboard to visualize stats pero country or globally",
+    url: "https://covid-dashboard-ceur.now.sh/",
   },
 ];
