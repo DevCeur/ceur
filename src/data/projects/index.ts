d17ae5ed6db0b2970172760115e7d4e30e7fc7ff
DevCeur <dev.carlos.umana@gmail.com>
2021-03-12 10:55:29 -0500

    [feat]: remove crashed project

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -6,11 +6,4 @@ export const projects = [
       "One of my biggest personal projects, a platform to find therapists, and book sessions with them.",
     url: "https://psychify.vercel.app/sign-in",
   },
-  {
-    id: 2,
-    name: "Covid Dashboard  &#128137;",
-    description:
-      "A simple covid dashboard to visualize stats pero country or globally",
-    url: "https://covid-dashboard-ceur.now.sh/",
-  },
 ];
